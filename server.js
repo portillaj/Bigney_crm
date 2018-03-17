f755d14fd2678460392e7014b94cbe018b66a03f
portillaj <jason.portilla85@gmail.com>
2018-03-17 07:29:01 -0400

    added test route

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
-  res.sedn({bye: 'Buddy'})
+  res.send({bye: 'Buddy'})
 });
 
 const PORT = process.env.PORT || 5000;
